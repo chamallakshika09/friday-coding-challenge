34ace6685b7b59379643dab06a6884dfc68740e8
Chamal Perera <chamallakshika09@gmail.com>
2021-03-17 22:40:05 +0530

    refine styles for links

--- src/components/styled-link/styles.tsx
+++ src/components/styled-link/styles.tsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 export const StyledLink = styled(Link)`
   color: black;
   flex: 0 0 21%;
-  margin: 20px;
-  height: 50px;
+  margin: 10px;
+  height: 20px;
   &:hover {
     color: blue;
   }
