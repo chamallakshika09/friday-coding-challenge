0bbf7024cee84de307f0bfb8c230d3f2adedf3e4
Chamal Perera <chamallakshika09@gmail.com>
2021-03-17 20:22:48 +0530

    change card style

--- src/components/card/styles.tsx
+++ src/components/card/styles.tsx
@@ -1,11 +1,10 @@
 import styled from 'styled-components';
 
 export const Card = styled.div`
-  min-width: 200px;
-  min-height: 250px;
-  max-width: 200px;
+  min-width: 150px;
+  min-height: 150px;
+  max-width: 150px;
   box-shadow: 0 4px 8px 0 gray;
   border: 2px solid black;
   margin: 10px;
-  cursor: pointer;
 `;
